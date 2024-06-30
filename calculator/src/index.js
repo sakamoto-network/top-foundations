fc361b7194c1712a75f1f349ce521dbf074269ef
Sakamoto <me@sakamoto.network>
2024-06-30 09:11:29 -0500

    fix actual calculator part
    this is buggy, extremely so, but i think it's time to finally move on.
    it *kind of* works.

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -1,78 +1,37 @@
 let a = new Number();
 let b = new Number();
-let enableOperators = false;
-let buffer = new String();
+let operator = new String();
+let operatorResult = new Number();
 
 let display = document.getElementById("display");
 let numbers = document.getElementsByClassName("calc-number");
 let operators = document.getElementsByClassName("calc-button");
 
-function enter() {
-  if (display.value == "" && enableOperators == false) {
-    display.placeholder = "Please enter a number!";
-  } else {
-    enableOperators = true;
-    b = display.value;
-    display.value = "";
-    display.placeholder = b;
-    for (let i = 1; i < 5; i++) {
-      operators[i].disabled = false;
-    }
-    buffer = "";
-    checkForAbsolutes();
-  }
-}
-
 // this is the only way i could get the function to display numbers properly
 function changeDisplay(num) {
   display.value += num.srcElement.innerHTML;
-  buffer = display.value;
-  checkForAbsolutes();
-}
-
-function operate(param) {
-  a = Number(display.value);
-  b = Math.round(Number(b));
-  if (param.srcElement.innerHTML === "+") {
-    b += Number(display.value);
-    display.value = "";
-    display.placeholder = b;
-  }
-  if (param.srcElement.innerHTML === "-") {
-    b -= Number(display.value);
-    display.value = "";
-    display.placeholder = b;
-  }
-  if (param.srcElement.innerHTML === "*") {
-    b *= Number(display.value);
-    display.value = "";
-    display.placeholder = b;
-  }
-  if (param.srcElement.innerHTML === "/") {
-    b /= Number(display.value);
-    display.value = "";
-    display.placeholder = b;
-  }
-  if (param.srcElement.innerHTML === "/" && a == 0 && buffer == 0) {
-    display.placeholder = null;
-  }
-  buffer = "";
   checkForAbsolutes();
 }
 
 function backspace() {
-  buffer = buffer.slice(0, -1);
-  display.value = buffer;
+  display.value = display.value.slice(0, -1);
+  checkForAbsolutes();
 }
 
 function checkForAbsolutes() {
-  if (buffer.includes(".") || (buffer.match(".") || []).length > 1) {
-    numbers[9].disabled = true;
+  if (
+    display.value.includes(".") ||
+    (display.value.match(".") || []).length > 1
+  ) {
+    numbers[10].disabled = true;
   } else {
-    numbers[9].disabled = false;
+    numbers[10].disabled = false;
   }
 
-  if (buffer[0] == "-" || (buffer.length > 0 && !buffer[0].includes("-"))) {
+  if (
+    display.value[0] == "-" ||
+    (display.value.length > 0 && !display.value[0].includes("-"))
+  ) {
     numbers[11].disabled = true;
   } else {
     numbers[11].disabled = false;
@@ -83,12 +42,83 @@ for (let i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener("click", changeDisplay);
 }
 
-operators[0].addEventListener("click", backspace);
-for (let i = 1; i < 5; i++) {
-  operators[i].addEventListener("click", operate);
+operators[0].addEventListener("click", () => {
+  clear();
+  checkForAbsolutes();
+});
+operators[1].addEventListener("click", backspace);
+for (let i = 2; i < 6; i++) {
+  operators[i].addEventListener("click", changeOperator);
 }
-operators[5].addEventListener("click", enter);
+operators[6].addEventListener("click", operate);
 
-for (let i = 1; i < 5; i++) {
-  operators[i].disabled = true;
+function changeOperator(button) {
+  if (operator === "+") {
+    operatorResult = a + Number(display.value);
+    display.value = "";
+    display.placeholder = operatorResult;
+  } else if (operator === "-") {
+    operatorResult = a - Number(display.value);
+    display.value = "";
+    display.placeholder = operatorResult;
+  } else if (operator === "*") {
+    operatorResult = a * Number(display.value);
+    display.value = "";
+    display.placeholder = operatorResult;
+  } else if (operator === "/") {
+    operatorResult = a / Number(display.value);
+    display.value = "";
+    display.placeholder = operatorResult;
+  } else {
+    a = Number(display.value);
+    display.value = "";
+    display.placeholder = a;
+  }
+
+  operator = button.currentTarget.innerText;
+  checkForAbsolutes();
+}
+
+function clear() {
+  a = new Number();
+  b = new Number();
+  operator = "";
+  display.value = "";
+  display.placeholder = 0;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate() {
+  b = Number(display.value);
+  operator === "+"
+    ? (result = add(operatorResult, b))
+    : operator === "-"
+      ? (result = subtract(operatorResult, b))
+      : operator === "*"
+        ? (result = multiply(operatorResult, b))
+        : operator === "/" && display.value != "0"
+          ? (result = divide(operatorResult, b))
+          : null;
+
+  if (b === 0 && a != 0) {
+    display.value = "";
+    display.placeholder = "nice try";
+  } else {
+    display.value = Math.round(result * 100) / 100;
+  }
 }
